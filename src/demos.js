087c6cdc913027fbecef47c10edf6f3db327e21e
xem <maxime.euziere@gmail.com>
2017-01-08 21:34:53 +0100

    alignment

--- src/demos.js
+++ src/demos.js
@@ -91,11 +91,11 @@ val: dw 6519              ; n = 160 * 256 / pi / 2 ; 0x1977`;
 m8trix.onclick = e => {
   bytes = [196,28,159,171,71,71,235,249];
   td_original.innerHTML = `<pre>S: les bx,[si] ; sets ES to the screen, assume si = 0x100 ; 0x101 is SBB AL,9F and changes the char ; without CR flag, there would be; no animation ;)
-lahf			; gets 0x02 (green) in the first run ; afterwards, it is not called again ; because of alignment ;)
-stosw			; print the green char ... ; (is also 0xAB9F and works as segment)
-inc di			; and skip one row
-inc di			;
-jmp short S+1   ; repeat on 0x101 `;
+lahf           ; gets 0x02 (green) in the first run ; afterwards, it is not called again ; because of alignment ;)
+stosw          ; print the green char ... ; (is also 0xAB9F and works as segment)
+inc di         ; and skip one row
+inc di         ;
+jmp short S+1  ; repeat on 0x101 `;
   cpu_mode = 32;
   disassemble();
 }
